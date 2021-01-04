cef8253391d5b166f698e831aeaceceb5671d895
Jiří Vrba <jiri@vrba.dev>
2021-01-04 15:13:07 +0100

    Initial commit

---
+++ src/main/java/dev/vrba/pyro/PyroApplication.java
@@ -0,0 +1,13 @@
+package dev.vrba.pyro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PyroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PyroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/vrba/pyro/PyroApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vrba.pyro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PyroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
