b97e572166573b83ae4e7813458ea3f26920a220
Jiří Vrba <jiri@vrba.dev>
2021-01-04 17:55:28 +0100

    Added DiscordBotService and its bootstrapping in Application entry point

--- src/main/java/dev/vrba/pyro/PyroApplication.java
+++ src/main/java/dev/vrba/pyro/PyroApplication.java
@@ -1,13 +1,39 @@
 package dev.vrba.pyro;
 
+import dev.vrba.pyro.discord.DiscordBotService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.security.auth.login.LoginException;
+import java.util.logging.Logger;
+
 @SpringBootApplication
-public class PyroApplication {
+public class PyroApplication implements CommandLineRunner {
+
+    private final DiscordBotService bot;
+
+    private final Logger logger;
+
+    @Autowired
+    public PyroApplication(final DiscordBotService bot, final Logger logger) {
+        this.bot = bot;
+        this.logger = logger;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(PyroApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) {
+        try { this.bot.start(); }
+        catch (LoginException exception) {
+            logger.severe(
+            "Cannot start the application.\n" +
+                "The provided DISCORD_TOKEN is invalid and cannot be used to login within the gateway."
+            );
+        }
+    }
 }
---
+++ src/main/java/dev/vrba/pyro/discord/DiscordBotService.java
@@ -0,0 +1,42 @@
+package dev.vrba.pyro.discord;
+
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.events.ReadyEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.security.auth.login.LoginException;
+import java.util.logging.Logger;
+
+@Service
+public class DiscordBotService {
+
+    private final String token;
+
+    private final Logger logger;
+
+    @Autowired
+    public DiscordBotService(@Value("$DISCORD_TOKEN") final String token, final Logger logger) {
+        this.token = token;
+        this.logger = logger;
+    }
+
+    public void start() throws LoginException {
+        logger.info("Starting the Pyro Discord bot service.");
+
+        JDABuilder.createDefault(token)
+                .addEventListeners(new ModulesLoader())
+                .build();
+    }
+
+    private class ModulesLoader extends ListenerAdapter {
+        @Override
+        public void onReady(@NotNull ReadyEvent event) {
+            logger.info("Logged into Discord Gateway.");
+            logger.info("Loading Pyro modules.");
+        }
+    }
+}
